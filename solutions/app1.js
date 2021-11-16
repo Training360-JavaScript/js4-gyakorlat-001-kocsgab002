76d8f4fdb17089c009dfede31a3610017bbd8f49
Kocsis Gábor <ifjkocsis.gabor@gmail.com>
2021-11-16 22:01:44 +0100

    all tasks done

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,11 @@
+const converterPromise = (array = []) => {
+    return new Promise((resolve, reject) => {
+        if (array.every(element => (typeof element === 'string'))) {
+            resolve((array).map(item => item.toLocaleUpperCase()));
+        } else {
+            reject('Error: Not all elements are string type!')
+        }
+    })
+};
 
-
-
-
-
-
-// export default converterPromise;
+ export default converterPromise;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,12 @@
+const sorterPromise = (array = []) => {
+    return new Promise((resolve, reject) => {
+        if (array.every(element => (typeof element === 'string'))) {
+            resolve((array).sort());
+        } else {
+            reject('Error: Not all elements are string type!')
+        }
+    })
+};
 
 
-
-
-
-
-// export default sorterPromise;
+export default sorterPromise
